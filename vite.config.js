a097697e163731cd79bade02b568fed946211876
RubenD582 <rdreyer523@gmail.com>
2025-02-22 14:38:30 +0200

    Added deployment files

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
-  base: '/',
+  base: 'daniela/',
   plugins: [react()],
   css: {
     postcss: {
